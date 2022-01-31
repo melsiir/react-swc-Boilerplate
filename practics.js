7c4eed5762a9a603fa1546af122bbe53ea51a4ce
Mohammed Elsir <likmik271@gmail.com>
2022-02-01 01:02:54 +0200

    added support for postcss and tailwindcss and added cache for faster rebuild

--- practics.js
+++ practics.js
@@ -1,10 +1,22 @@
+
+const { timeEnd } = require('console');
 const fs = require('fs');
 const path = require('path')
 
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-const pathi = resolveApp('build')
-console.log(appDirectory)
-console.log(pathi)
+// const appDirectory = fs.realpathSync(process.cwd());
+// const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+// const pathi = resolveApp('build')
+// console.log(appDirectory)
+// console.log(pathi)
+// const start = new Date().getTime()
+console.time('st')
+// const Dir = fs.realpathSync(process.cwd());
+// const joinPath = (...paths) => path.join(Dir, ...paths) 
+const joinPath = (...paths) => path.join(__dirname, ...paths) 
 
-
+const ts = joinPath('yi', 'jehs', 'index.js')
+console.timeEnd('st')
+// const end = new Date().getTime()
+// console.log(end - start)
+console.log(ts)
+// console.log(start)
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,81 @@
+console.time("a");
 const path = require("path");
-const fs = require('fs')
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const createEnviromentHash = require("./utils/createEnviromentHash");
 const isDevelopment = process.env.NODE_ENV === "development";
 const isProduction = process.env.NODE_ENV === "production";
+const isAnalyze = process.env.NODE_ENV === "analyze";
+const joinPath = (...paths) => path.join(__dirname, ...paths);
+const isTypeScript = fs.existsSync("./src/index.tsx");
+const isTailwind = fs.existsSync(joinPath('tailwind.config.js'))
+const imageInlineSizeLimit = parseInt(
+  process.env.IMAGE_INLINE_SIZE_LIMIT || '10000'
+);
+console.time("b");
+
 
 let mode = "development";
+const ts = joinPath("src", "index.tsx");
+const js = joinPath("src", "index.js");
 
 const styleLoader = isDevelopment
   ? "style-loader"
   : MiniCssExtractPlugin.loader;
+
+
+const postcssLoader = {
+        // Options for PostCSS as we reference these options twice
+        // Adds vendor prefixing based on your specified browser support in
+        // package.json
+        loader: require.resolve('postcss-loader'),
+        options: {
+          postcssOptions: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebook/create-react-app/issues/2677
+            ident: 'postcss',
+            config: false,
+            plugins: !isTailwind
+              ? [
+                  'postcss-flexbugs-fixes',
+                  [
+                    'postcss-preset-env',
+                    {
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    },
+                  ],
+                  // Adds PostCSS Normalize as the reset css with default options,
+                  // so that it honors browserslist config in package.json
+                  // which in turn let's users customize the target behavior as per their needs.
+                  'postcss-normalize',
+                ]
+              : [
+                  'tailwindcss',
+                  'postcss-flexbugs-fixes',
+                  [
+                    'postcss-preset-env',
+                    {
+                      autoprefixer: {
+                        flexbox: 'no-2009',
+                      },
+                      stage: 3,
+                    },
+                  ],
+                ],
+          },
+          sourceMap: isProduction ? false : isDevelopment,
+        },
+      }
 
 const plugins = [
   new HtmlWebpackPlugin({
@@ -22,19 +83,30 @@ const plugins = [
     inject: true,
     template: path.resolve(__dirname, "public/index.html"),
     // favicon: "./public/favicon.ico",
-    manifest: "./public/manifest.json", 
+    manifest: "./public/manifest.json",
+    minify: isProduction
+      ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        }
+      : {},
   }),
   new MiniCssExtractPlugin({
     filename: "static/css/[name].[contenthash:8].css",
     chunkFilename: "static/css/[id].[contenthash:8].css",
     ignoreOrder: true, // Enable to remove warnings about conflicting order
   }),
-  // new BundleAnalyzerPlugin(),
   // new CompressionPlugin()
 ];
 
-const isTypeScript = fs.existsSync('./src/index.ts')
-const isJavaScript = fs.existsSync('./src/index.js')
 
 
 
@@ -42,48 +114,84 @@ if (isDevelopment) {
   plugins.push(new ReactRefreshPlugin());
 }
 
-if (isProduction) {
+if (isProduction || isAnalyze) {
   mode = "production";
   //clean only in production mode
-  plugins.push(new CleanWebpackPlugin())
+  plugins.push(new CleanWebpackPlugin());
 }
 
+if (isAnalyze) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 const optimize = () => {
   if (isDevelopment) return undefined;
   return {
     minimize: isProduction,
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
     splitChunks: {
-       cacheGroups: {
-         vendor: {
-           test: /[\\/]node_modules[\\/]/,
-           name: 'vendors',
-           chunks: 'all',
-         },
-       },
-     },
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+      },
+    },
     minimizer: [
       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
       `...`,
       new CssMinimizerPlugin(),
     ],
+  };
+};
 
-  }
-}
-
-
-
-
-const swcConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '.swcrc'), 'utf-8'));
+const swcConfig = JSON.parse(
+  fs.readFileSync(path.join(__dirname, ".swcrc"), "utf-8")
+);
+swcConfig.jsc.parser = isTypeScript
+  ? {
+      syntax: "typescript",
+      tsx: true,
+      decorators: true,
+      dynamicImport: true,
+    }
+  : {
+      syntax: "ecmascript",
+      jsx: true,
+    };
 swcConfig.jsc.transform.react.development = isDevelopment;
 swcConfig.sourceMaps = isDevelopment;
 swcConfig.minify = !isDevelopment;
+swcConfig.jsc.minify = isDevelopment
+  ? {}
+  : {
+      compress: {
+        unused: true,
+      },
+      mangle: true,
+    };
 
+console.log(swcConfig);
 
-// console.log(swcConfig)
+const registerShutdown = (fn) => {
+  let run = false;
 
+  const wrapper = () => {
+    if (!run) {
+      run = true;
+      fn();
+    }
+  };
+
+  process.on("SIGINT", wrapper);
+  process.on("SIGTERM", wrapper);
+  process.on("exit", wrapper);
+};
 
 module.exports = {
   mode: mode,
@@ -121,14 +229,14 @@ module.exports = {
       path.posix.resolve(__dirname, "node_modules"),
     ],
   },
-  entry: path.join(__dirname, "/src/index.js"),
+  entry: isTypeScript ? ts : js,
   output: {
-    filename: isProduction ? "static/js/[name].[contenthash:8].js" :  "static/js/bundle.js",
+    filename: isDevelopment
+      ? "static/js/bundle.js"
+      : "static/js/[name].[contenthash:8].js",
     path: isDevelopment
       ? path.join(__dirname, "public")
-      : isProduction
-      ? path.join(__dirname, "build")
-      : "none",
+      : path.join(__dirname, "build"),
     pathinfo: isDevelopment,
     // clean: true,
   },
@@ -138,7 +246,7 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|js)x?$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         exclude: /(node_modules)/,
         use: {
           loader: "swc-loader",
@@ -149,28 +257,78 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         include: path.resolve(__dirname, "src"),
-        use: [styleLoader, "css-loader"],
+        use: [styleLoader, "css-loader", postcssLoader],
       },
       {
         test: /\.s[ca]ss$/i,
         exclude: /node_modules/,
         include: path.resolve(__dirname, "src"),
-        use: [styleLoader, "css-loader", "sass-loader"],
+        use: [styleLoader, "css-loader", "sass-loader", postcssLoader],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif|ico|txt)$/i,
+        test: /\.svg$/,
+        use: [
+          {
+            loader: require.resolve("@svgr/webpack"),
+            options: {
+              prettier: false,
+              svgo: false,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
+          },
+          {
+            loader: require.resolve("file-loader"),
+            options: {
+              name: "static/media/[name].[hash].[ext]",
+            },
+          },
+        ],
+        issuer: {
+          and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|ico|txt)$/i,
         // use: ["file-loader?name=[name].[ext]"],
         type: "asset/resource",
+        parser: {
+                dataUrlCondition: {
+                  maxSize: imageInlineSizeLimit,
+                },
+              },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/inline',
+        type: "asset/inline",
       },
     ],
+  },
+  cache: {
+    type: "filesystem",
+    version: createEnviromentHash(process.env.NODE_ENV),
+    cacheDirectory: joinPath("node_modules", ".cache"),
+    store: "pack",
+    buildDependencies: {
+      defaultWebpack: ["webpack/lib/"],
+      config: [__filename],
+    },
   },
   optimization: optimize(),
   // be carefull with devtool as they cost resource specialy source-map but off good debug in devtool
   // devtool: "source-map",
-    devtool: 'eval-cheap-module-source-map',
-
+  devtool: "eval-cheap-module-source-map",
 };
+
+registerShutdown(() => {
+  process.on("SIGINT", () => {
+    devServer.close();
+    process.exit(0);
+  });
+});
+
+console.timeEnd("b");
+console.timeEnd("a");
