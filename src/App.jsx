fb9824b7a392e7b96fa5fc43d2585dfcd1972af3
Mohammed Elsir <likmik271@gmail.com>
2022-02-05 15:51:57 +0200

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import logo from './media/logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="main">
-    <h1>hohha</h1>
-     <div>say jest , keee iso ok donkie5 days</div>
-    <div className='svg'>ok</div>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    <p>it seem like it work does it iiook? </p>
+    <div className="App">
+      <header className="App-header">
+       <div className='logo'></div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
-export default App
+export default App;
