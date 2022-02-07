30f1ddb67f5b8d1fe39314e6198b8e05a364da83
Mohammed Elsir <likmik271@gmail.com>
2022-02-07 22:00:49 +0200

    update installation script

--- bin/generate.mjs
+++ bin/generate.mjs
@@ -3,7 +3,6 @@ import { execSync } from "child_process";
 import * as readline from "node:readline/promises";
 import { createRequire } from "module";
 
-import chalk from "chalk";
 import path from "path";
 import fs from "fs";
 const rl = readline.createInterface({
